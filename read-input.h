7b0044e8f1a08caa6f7211295b6837486b3a9ee3
Allison-Q <41588956+Allison-Q@users.noreply.github.com>
2021-05-17 23:23:14 +0800

    Update read-input.h

--- read-input.h
+++ read-input.h
@@ -1,11 +1,11 @@
-// This module reads input and gives the corresponding ha_comp
+// This module turns the input into the corresponding ha_comp
 
 #include "high-accuracy-complex.h"
 
 
-// read_input(num) reads input and returns the corresponding ha_comp
+// read_input(num) turns num into the corresponding ha_comp
 // note: if num is an invalid complex (real) number, then return NULL and print
 //         an error message
 // effects: may allocate memory (client must call ha_comp_destroy)
-// time: O(n) where n is the length of num
-struct ha_comp *read_input(void); 
+// time: O(n)
+struct ha_comp *read_input(char *num); 
